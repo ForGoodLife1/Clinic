32120c896a2dfb5ae1abdaf7bc785681c14ec7a5
forgoodlife1 <forgoodlife78@gmail.com>
2024-08-26 13:27:18 +0300

    first commit

---
+++ Clinic.Model/Model/Prescription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clinic.Core.Model;
+
+public partial class Prescription
+{
+    public int PrescriptionId { get; set; }
+
+    public int MedicalRecordId { get; set; }
+
+    public string MedicationName { get; set; } = null!;
+
+    public string Dosage { get; set; } = null!;
+
+    public string Frequency { get; set; } = null!;
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public string? SpecialInstructions { get; set; }
+
+    public virtual MedicalRecord MedicalRecord { get; set; } = null!;
+}
---
+++ Clinic/Program.cs
@@ -0,0 +1,35 @@
+using Clinic.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ClinicDbContext>(Options =>
+        Options.UseSqlServer(builder.Configuration.GetConnectionString("constr"),
+             b => b.MigrationsAssembly(typeof(ClinicDbContext).Assembly.FullName)));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
